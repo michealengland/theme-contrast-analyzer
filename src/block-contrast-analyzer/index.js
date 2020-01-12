82d9d3cbdb31a1743c72d98796265151be322dbd
Mike England <mike.england@webdevstudios.com>
2020-01-12 12:50:07 -0500

    Import Save
    Import data moved from the save function to Save.

--- src/block-contrast-analyzer/index.js
+++ src/block-contrast-analyzer/index.js
@@ -3,6 +3,7 @@
  */
 import ThemeSwatchAnalyze from './swatch-contrast';
 import Edit from './edit';
+import Save from './save';
 
 const { __ } = wp.i18n;
 const { Fragment } = wp.element;
@@ -21,65 +22,6 @@ registerBlockType( 'tca/swatch', {
 		},
 	},
 	Edit,
-	save: ( { className } ) => {
-
-		// Get theme color data.
-		const themeColors = select( 'core/editor' ).getEditorSettings().colors;
-
-		// Assign an empty array.
-		let colorPairs = [];
-
-		// Loop through theme color palette.
-		for ( let i = 0; i < themeColors.length - 1; i++ ) {
-			colorPairs.push( themeColors[ i ] );
-		}
-
-		return (
-			<ul
-				className={ className }
-			>
-				{ themeColors.map( ( value, index ) => {
-					// Create filtered array.
-					const results = colorPairs.filter( ( pair ) => ( pair.slug ) !== value.slug );
-
-					return (
-						// Outputs a list of each theme color.
-						<li
-							className={ value.slug }
-							key={ index }
-						>
-								<span
-									className={ 'primary-color-label' }
-								>
-									{ 'Foreground: ' + value.name }
-								</span>
-
-							<ul
-								className={ 'color-pairs' }
-							>
-								{ results.map( ( pair, key ) => {
-									return (
-										<li
-											key={ key }
-										>
-											{ 'Background: ' + pair.name }
-											<ThemeSwatchAnalyze
-												{ ...{
-													fontSize: 18,
-													swatchColorName: pair.name,
-													swatchPrimaryColor: value.color,
-													swatchSecondaryColor: pair.color,
-												} }
-											/>
-										</li>
-									);
-								} ) }
-							</ul>
-						</li>
-					);
-				} ) }
-			</ul>
-		);
-	},
+	Save,
 } );
 
