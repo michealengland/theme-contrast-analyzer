b729bd97dc571aceb44b2f5e0c33d020969cb071
Mike England <mike.england@webdevstudios.com>
2020-01-12 12:51:05 -0500

    Create save.js
    Setup default export for the save function from index. Then re-added
    functionality.

---
+++ src/block-contrast-analyzer/save.js
@@ -0,0 +1,66 @@
+const { __ } = wp.i18n;
+const { Fragment } = wp.element;
+const { registerBlockType } = wp.blocks;
+const { select } = wp.data;
+
+const Save = ( props, className ) => {
+	// Get theme color data.
+	const themeColors = select( 'core/editor' ).getEditorSettings().colors;
+
+	// Assign an empty array.
+	let colorPairs = [];
+
+	// Loop through theme color palette.
+	for ( let i = 0; i < themeColors.length - 1; i++ ) {
+		colorPairs.push( themeColors[ i ] );
+	}
+
+	return (
+		<ul
+			className={ className }
+		>
+			{ themeColors.map( ( value, index ) => {
+				// Create filtered array.
+				const results = colorPairs.filter( ( pair ) => ( pair.slug ) !== value.slug );
+
+				return (
+					// Outputs a list of each theme color.
+					<li
+						className={ value.slug }
+						key={ index }
+					>
+							<span
+								className={ 'primary-color-label' }
+							>
+								{ 'Foreground: ' + value.name }
+							</span>
+
+						<ul
+							className={ 'color-pairs' }
+						>
+							{ results.map( ( pair, key ) => {
+								return (
+									<li
+										key={ key }
+									>
+										{ 'Background: ' + pair.name }
+										<ThemeSwatchAnalyze
+											{ ...{
+												fontSize: 18,
+												swatchColorName: pair.name,
+												swatchPrimaryColor: value.color,
+												swatchSecondaryColor: pair.color,
+											} }
+										/>
+									</li>
+								);
+							} ) }
+						</ul>
+					</li>
+				);
+			} ) }
+		</ul>
+	);
+}
+
+export default Save;
