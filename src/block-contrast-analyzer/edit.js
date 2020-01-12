52e98a10e431fa40ec41e9b945077ca0614ce298
Mike England <mike.england@webdevstudios.com>
2020-01-12 12:47:11 -0500

    Create edit function
    Incorporated edit functionality inside of a const with export for index.js.

---
+++ src/block-contrast-analyzer/edit.js
@@ -0,0 +1,67 @@
+const { __ } = wp.i18n;
+const { Fragment } = wp.element;
+const { registerBlockType } = wp.blocks;
+const { select } = wp.data;
+
+
+const Edit = ( props, className ) => {
+	// Get theme color data.
+	const themeColors = select( 'core/editor' ).getEditorSettings().colors;
+
+	// Assign an empty array.
+	let colorPairs = [];
+
+	// Loop through theme color palette.
+	for ( let i = 0; i < themeColors.length - 1; i++ ) {
+		colorPairs.push( themeColors[ i ] );
+	}
+
+	return (
+		<Fragment>
+			<ul
+				className={ className }
+			>
+				{ themeColors.map( ( value, index ) => {
+					// Allow all non matching colors.
+					const results = colorPairs.filter( ( pair ) => ( pair.slug ) !== value.slug );
+
+					return (
+						// Outputs a list of each theme color.
+						<li
+							className={ value.slug }
+							key={ index }
+						>
+							<span
+								className={ 'primary-color-label' }
+							>
+								{ value.name }
+							</span>
+
+							<ul className={ 'color-pairs' } >
+								{ results.map( ( pair, key ) => {
+									return (
+										<li
+											key={ key }
+										>
+											{ pair.name }
+											<ThemeSwatchAnalyze
+												{ ...{
+													fontSize: 18,
+													swatchColorName: pair.name,
+													swatchPrimaryColor: value.color,
+													swatchSecondaryColor: pair.color,
+												} }
+											/>
+										</li>
+									);
+								} ) }
+							</ul>
+						</li>
+					);
+				} ) }
+			</ul>
+		</Fragment>
+	);
+}
+
+export default Edit;
