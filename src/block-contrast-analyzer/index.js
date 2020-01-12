09a0fa164e45a5af2cdd9f7717c2d9d36e42f922
Mike England <mike.england@webdevstudios.com>
2020-01-12 12:45:27 -0500

    Move move edit function outside index
    Moved the edit function to it's own file and imported to clean up the
    index.

--- src/block-contrast-analyzer/index.js
+++ src/block-contrast-analyzer/index.js
@@ -2,6 +2,7 @@
  * Internal dependencies
  */
 import ThemeSwatchAnalyze from './swatch-contrast';
+import Edit from './edit';
 
 const { __ } = wp.i18n;
 const { Fragment } = wp.element;
@@ -19,66 +20,7 @@ registerBlockType( 'tca/swatch', {
 			selector: 'div',
 		},
 	},
-	edit: ( { className } ) => {
-
-		// Get theme color data.
-		const themeColors = select( 'core/editor' ).getEditorSettings().colors;
-
-		// Assign an empty array.
-		let colorPairs = [];
-
-		// Loop through theme color palette.
-		for ( let i = 0; i < themeColors.length - 1; i++ ) {
-			colorPairs.push( themeColors[ i ] );
-		}
-
-		return (
-			<Fragment>
-				<ul
-					className={ className }
-				>
-					{ themeColors.map( ( value, index ) => {
-						// Allow all non matching colors.
-						const results = colorPairs.filter( ( pair ) => ( pair.slug ) !== value.slug );
-
-						return (
-							// Outputs a list of each theme color.
-							<li
-								className={ value.slug }
-								key={ index }
-							>
-								<span
-									className={ 'primary-color-label' }
-								>
-									{ value.name }
-								</span>
-
-								<ul className={ 'color-pairs' } >
-									{ results.map( ( pair, key ) => {
-										return (
-											<li
-												key={ key }
-											>
-												{ pair.name }
-												<ThemeSwatchAnalyze
-													{ ...{
-														fontSize: 18,
-														swatchColorName: pair.name,
-														swatchPrimaryColor: value.color,
-														swatchSecondaryColor: pair.color,
-													} }
-												/>
-											</li>
-										);
-									} ) }
-								</ul>
-							</li>
-						);
-					} ) }
-				</ul>
-			</Fragment>
-		);
-	},
+	Edit,
 	save: ( { className } ) => {
 
 		// Get theme color data.
